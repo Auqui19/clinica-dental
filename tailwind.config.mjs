f1cfece50ad24c7205c0082af9b8928ce67d3b8f
Alonso Auqui <andriod.pvp@gmail.com>
2024-07-09 11:48:14 -0500

    📍 Se mejoro navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-gray': '#F8F8F8',
+				'secondary-teal': '#00B4B5',
+				'secondary-green': '#24B89F',
+				'secondary-black': '#141414',
+				'secondary-bluewhite': '#E8F1F6',
+			},
+		},
 	},
 	plugins: [],
 }
